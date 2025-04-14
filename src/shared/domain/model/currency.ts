23f8b565ae2ea6a6f0a0569df47db6799f065bff
Ro <u202213765@upc.edu.pe>
2025-04-14 11:04:14 -0500

    feat: add currency type.

---
+++ src/shared/domain/model/currency.ts
@@ -0,0 +1,28 @@
+type UppercaseLetter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J";
+
+export type CurrencyCode = `${UppercaseLetter}${UppercaseLetter}${UppercaseLetter}${UppercaseLetter}`;
+
+export class Currency {
+    private readonly _code: CurrencyCode;
+
+    constructor(code: CurrencyCode) {
+        this._code = code;
+    }
+
+    public get code(): string {
+        return this._code;
+    }
+
+    public formatAmount = (amount: number, locale = 'en-US'): string => {
+        return amount.toLocaleString(locale, {
+            style: 'currency',
+            currency: 'en-US',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        });
+    };
+
+
+    public toString = (): string => {this.code }
+}
+
